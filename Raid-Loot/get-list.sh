cf3d974f112872547381028faa5f8820047465b4
Iain J Gardiner <iain@firelightning.com>
2020-08-11 18:18:39 +0100

    Removed the  opting instead to expand the 'grep -E' command in full and split out into separate lines per pattern to aid readbility. Also, split the awk command to separate lines for readability and commented out the final 'rm' while testing.

--- Raid-Loot/get-list.sh
+++ Raid-Loot/get-list.sh
@@ -8,15 +8,9 @@ DATESTAMP="$(date +%Y-%m-%d-%H%M%S)"
 URL="${1}"
 TEMPFILE="${URL##*/}-${DATESTAMP}"      # Greedily trim everything from front (##) matching any character (*) until last occurring "/"
 TEMPTRIMFILE="${TEMPFILE}-trimmed"
-TEMPEGREPSCRIPT="${TEMPFILE}-egrep"
 TEMPAWKFILE="${TEMPFILE}-awk"
 FINALFILE="${TEMPFILE}-final"
 PYTHONSCRIPT="get-html-element.py"
-
-#Populate egrep file:
-echo "<h3 class=\"heading-size-3\">([a-zA-Z ,']+)<\/h3>" > "${TEMPEGREPSCRIPT}"
-echo "<h2 class=\"heading-size-2\">([a-zA-Z ,']+) Trash Loot<\/h2>" >> "${TEMPEGREPSCRIPT}"
-echo "<a href=\"\/item=[0-9]+\">([a-zA-Z ,']+)<\/a>" >> "${TEMPEGREPSCRIPT}"
 
 # Retrieve the page with cURL and output to $TEMPFILE:
 curl -o "${TEMPFILE}" "${URL}"
@@ -25,10 +19,13 @@ curl -o "${TEMPFILE}" "${URL}"
 python3 "${PYTHONSCRIPT}" "${TEMPFILE}" > "${TEMPTRIMFILE}"
 
 # Extract loot source headers and item hyperlinks from $TEMPTRIMFILE and write back into $TEMPFILE:
-egrep -o -f "${TEMPEGREPSCRIPT}" "${TEMPTRIMFILE}" | sed -n -e '/<h3 /,$p' > "${TEMPFILE}"
+grep -o -E "<h3 class=\"heading-size-3\">([a-zA-Z ,']+)<\/h3>\
+|<h2 class=\"heading-size-2\">([a-zA-Z ,']+) Trash Loot<\/h2>\
+|<a href=\"\/item=[0-9]+\">([a-zA-Z ,']+)<\/a>" "${TEMPTRIMFILE}" | sed -n -e '/<h3 /,$p' > "${TEMPFILE}"
 
 # Rewrite each line to strip out the HTML and format the info:
-gawk -F [\>\<=\"] -e '/\<h3|\<h2/ { gsub(/Classic | Loot/, ""); print $6 }; /\<a/ { formattedName = tolower(print $7); print $7 ";https://classic.wowhead.com/item=" $5 "/" $formattedName }' "${TEMPFILE}" > "${TEMPAWKFILE}"
+gawk -F [\>\<=\"] -e '/\<h3|\<h2/ { gsub(/Classic | Loot/, ""); print $6 }' \
+	-e '/\<a/ { print $5 ";" $7 ";https://classic.wowhead.com/item=" $5 "/" }' "${TEMPFILE}" > "${TEMPAWKFILE}"
 
 # Tidy up:
-rm "${TEMPFILE}" "${TEMPTRIMFILE}" "${TEMPEGREPSCRIPT}" "${TEMPAWKFILE}"
+#rm "${TEMPFILE}" "${TEMPTRIMFILE}" "${TEMPAWKFILE}"
