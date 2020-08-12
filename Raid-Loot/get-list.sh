b003423edf67f96b4230da0a8083f7e228cb33ee
Iain J Gardiner <iain@firelightning.com>
2020-08-12 12:04:13 +0100

    Amended regex patterns to match additional characters. Added code to split the output into separate files  named 'Boss1', 'Boss2', etc.

--- Raid-Loot/get-list.sh
+++ Raid-Loot/get-list.sh
@@ -21,14 +21,21 @@ python3 "${PYTHONSCRIPT}" "${TEMPFILE}" > "${TEMPTRIMFILE}"
 # Extract loot source headers and item hyperlinks from $TEMPTRIMFILE and write back into $TEMPFILE:
 grep -o -E "<h3 class=\"heading-size-3\">([a-zA-Z ,']+)<\/h3>\
 |<h2 class=\"heading-size-2\">([a-zA-Z ,']+) Trash Loot<\/h2>\
-|<a href=\"\/item=[0-9]+\">([a-zA-Z ,']+)<\/a>" "${TEMPTRIMFILE}" | sed -n -e '/<h3 /,$p' > "${TEMPFILE}"
+|<a href=\"\/item=[0-9]+\">([a-zA-Z ,':-]+)<\/a>" "${TEMPTRIMFILE}" | sed -n -e '/<h3 /,$p' > "${TEMPFILE}"
 
 # Rewrite each line to strip out the HTML and format the info:
 gawk -F [\>\<=\"] -e '/\<h3|\<h2/ { gsub(/Classic | Loot/, ""); print $6 }' \
 	-e '/\<a/ { formattedName = tolower($7); \
 		formattedName = gensub(/ /, "-", "g", formattedName); \
-		formattedName = gensub(/[,\047]/, "", "g", formattedName); \
+		formattedName = gensub(/[:,\047]/, "", "g", formattedName); \
+		formattedName = gensub(/-{3}/, "-", "g", formattedName); \
 		print $5 ";" $7 ";https://classic.wowhead.com/item=" $5 "/" formattedName }' "${TEMPFILE}" > "${TEMPAWKFILE}"
+
+# Split $TEMPAWKFILE into separate files for each boss:
+awk -v i=0 -e '{ if (!/;/) i++; print $0 >> "Boss"i }' "${TEMPAWKFILE}"
+
+# Create directory for the "Boss*" files and move them in:
+
 
 # Tidy up:
 #rm "${TEMPFILE}" "${TEMPTRIMFILE}" "${TEMPAWKFILE}"
